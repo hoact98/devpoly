1ac69028558996b23961feffbd647dbdd929f68e
Hoa98 <chuthihoa98bgg@gmail.com>
2021-06-20 23:08:14 +0700

    m

--- public/js/resources_js_pages_admin_auth_Login_vue.js
+++ public/js/resources_js_pages_admin_auth_Login_vue.js
@@ -1111,7 +1111,9 @@ var render = function() {
                   }),
                   _vm._v(" "),
                   _c("label", { attrs: { for: "remember" } }, [
-                    _vm._v("\n                  Remember Me\n                ")
+                    _vm._v(
+                      "\r\n                  Remember Me\r\n                "
+                    )
                   ])
                 ])
               ]),
