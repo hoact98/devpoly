19a81cf296c8147c9b415902e411fc229a0b1326
Ha Minh Nghia <nghiahm@techvify.com.vn>
2021-07-05 00:58:17 +0700

    thaind/feedback

--- resources/js/pages/public/Feedback.vue
+++ resources/js/pages/public/Feedback.vue
@@ -14,12 +14,12 @@
                                     <div class="feedback-content">
                                         <div class="row">
                                             <div class="col-lg-3 col-md-12 col-md-12">
-                                                <div class="upvote button-block">
+                                                <!-- <div class="upvote button-block">
                                                     <div class="icon">
                                                         <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"></path></svg>
                                                     </div>
                                                     <span>upvote</span>
-                                                </div>
+                                                </div> -->
 
                                                 <div class="feedback-user-section">
                                                     <div>
@@ -30,12 +30,12 @@
 
                                                             <div>
                                                                 <p class="feedback-user-name">Kevin Rodriguez</p>
-                                                                <span class="feedback-user-reputation">10  reputations</span>
+                                                                <span class="feedback-user-reputation">Memtor</span>
                                                             </div>
                                                         </div>
                                                     </div>
 
-                                                    <div class="user-skill-wrapper">
+                                                    <!-- <div class="user-skill-wrapper">
                                                         <div class="skill-list">
                                                             <button class="btn-feedback">
                                                                 <span class="">Mentoring</span>
@@ -47,7 +47,7 @@
                                                             </button>
                                                         </div>
                                                         <span>more</span>
-                                                    </div>
+                                                    </div> -->
                                                 </div>
                                             </div>
                                             <div class="col-lg-9 col-md-12">
@@ -75,12 +75,12 @@
                                                 <div class="row">
                                                     <div class="col-lg-3 col-md-12">
                                                         <div class="">
-                                                            <div class="upvote button-block">
+                                                            <!-- <div class="upvote button-block">
                                                                 <div class="icon">
                                                                     <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"></path></svg>
                                                                 </div>
                                                                 <span>upvote</span>
-                                                            </div>
+                                                            </div> -->
 
                                                             <div class="reply-feedback-user-info">
                                                                 <div class="feedback-user-img">
@@ -89,7 +89,7 @@
 
                                                                 <div>
                                                                     <p class="feedback-user-name">Kevin Rodriguez</p>
-                                                                    <span class="feedback-user-reputation">10  reputations</span>
+                                                                    <span class="feedback-user-reputation">User</span>
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -134,181 +134,6 @@
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-9 col-md-9 col-xs-9 col-lg-9 col-xl-9 feedback-wrapper">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="feedback-content">
-                                        <div class="row">
-                                            <div class="col-lg-3 col-md-12 col-md-12">
-                                                <div class="upvote button-block">
-                                                    <div class="icon">
-                                                        <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"></path></svg>
-                                                    </div>
-                                                    <span>upvote</span>
-                                                </div>
-
-                                                <div class="feedback-user-section">
-                                                    <div>
-                                                        <div class="feedback-user-info">
-                                                            <div class="feedback-user-img">
-                                                                <img src="https://avatars.githubusercontent.com/u/48230483?v=4" alt="">
-                                                            </div>
-
-                                                            <div>
-                                                                <p class="feedback-user-name">Kevin Rodriguez</p>
-                                                                <span class="feedback-user-reputation">10  reputations</span>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="user-skill-wrapper">
-                                                        <div class="skill-list">
-                                                            <button class="btn-feedback">
-                                                                <span class="">Mentoring</span>
-                                                                <svg class="MuiSvgIcon-root icon-feedback" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"></path></svg>
-                                                            </button>
-                                                            <button class="btn-feedback">
-                                                                <span class="">Mentoring</span>
-                                                                <svg class="MuiSvgIcon-root icon-feedback" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"></path></svg>
-                                                            </button>
-                                                        </div>
-                                                        <span>more</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-9 col-md-12">
-                                                <div class="feedback-detail">
-                                                    <p>It looks dope! Just a small suggestion, try to create the "join" element like a button or change its cursor to the pointer. Also, give validations if we try to put invalid style email id.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-12">
-                                    <div class="feedback-time">
-                                        <span>an hour ago</span>
-                                    </div>
-                                </div>
-
-
-
-                                <div>
-                                    <div class="col-12">
-                                        <div class="row">
-                                            <div class="col-1">
-
-                                            </div>
-                                            <div class="col-11 reply-feedback">
-                                                <div class="row">
-                                                    <div class="col-lg-3 col-md-12">
-                                                        <div class="">
-                                                            <div class="upvote button-block">
-                                                                <div class="icon">
-                                                                    <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"></path></svg>
-                                                                </div>
-                                                                <span>upvote</span>
-                                                            </div>
-
-                                                            <div class="reply-feedback-user-info">
-                                                                <div class="feedback-user-img">
-                                                                    <img src="https://avatars.githubusercontent.com/u/48230483?v=4" alt="">
-                                                                </div>
-
-                                                                <div>
-                                                                    <p class="feedback-user-name">Kevin Rodriguez</p>
-                                                                    <span class="feedback-user-reputation">10  reputations</span>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="col-lg-9 col-md-12">
-                                                        <div class="reply-feedback-detail">
-                                                            <p>It looks dope! Just a small suggestion, try to create the "join" element like a button or change its cursor to the pointer. Also, give validations if we try to put invalid style email id.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-12">
-                                        <div class="feedback-time">
-                                            <span>54 minutes ago</span>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div>
-                                    <div class="col-12">
-                                        <div class="row">
-                                            <div class="col-1">
-
-                                            </div>
-                                            <div class="col-11 reply-feedback">
-                                                <div class="row">
-                                                    <div class="col-lg-3 col-md-12">
-                                                        <div class="">
-                                                            <div class="upvote button-block">
-                                                                <div class="icon">
-                                                                    <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"></path></svg>
-                                                                </div>
-                                                                <span>upvote</span>
-                                                            </div>
-
-                                                            <div class="reply-feedback-user-info">
-                                                                <div class="feedback-user-img">
-                                                                    <img src="https://avatars.githubusercontent.com/u/48230483?v=4" alt="">
-                                                                </div>
-
-                                                                <div>
-                                                                    <p class="feedback-user-name">Kevin Rodriguez</p>
-                                                                    <span class="feedback-user-reputation">10  reputations</span>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="col-lg-9 col-md-12">
-                                                        <div class="reply-feedback-detail">
-                                                            <p>It looks dope! Just a small suggestion, try to create the "join" element like a button or change its cursor to the pointer. Also, give validations if we try to put invalid style email id.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-12">
-                                        <div class="feedback-time">
-                                            <span>54 minutes ago</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <div class="col-3 col-md-3 col-xs-3 col-lg-3 col-xl-3">
-                            <div class="challenge-info">
-                                <div class="author-info">
-                                    <div class="author-avatar">
-                                        <img class="author-image" src="https://avatars.githubusercontent.com/u/14989491?v=4" alt="">
-                                    </div>
-                                    <p class="author-name">Saad Raja</p>
-                                </div>
-
-                                <div>
-                                    <img class="challenge-image" src="images/challenge-img-1.png" alt="">
-                                </div>
-                                <div class="challenge-title">
-                                    <p>404 NOT FOUND</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </section>
 </template> 
