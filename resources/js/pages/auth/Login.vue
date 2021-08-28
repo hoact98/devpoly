2d63f243699db90ab6969f9e261759998fc5857b
Ngọc Ninh Tạ <ngocninhta@Ngocs-MacBook-Pro.local>
2021-08-28 18:39:12 +0700

    new update

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -92,7 +92,7 @@ export default {
         Cookies.remove('intended_url')
         this.$router.push({ path: intendedUrl })
       } else {
-        this.$router.push({ name: 'profile' })
+        this.$router.push({ name: 'home' })
       }
     }
   }  
